95e322c2671ca1431a56e316be50c06db7d321b8
Henry Calderon <theceintiant@gmail.com>
2020-02-20 22:58:15 -0800

    Completed ch 4.

--- Habitual/Views/HabitsTableViewController.swift
+++ Habitual/Views/HabitsTableViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class HabitsTableViewController: UITableViewController {
+    var names: [String] = ["Alan", "Braus", "Adriana", "Mitchell", "Dani", "Jess", "Dan", "Meredith", "Dan", "Milad"]
+    // return the number of rows for the given section
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    // return the UITableViewCell for the given indexPath
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        if let dequeueCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell = dequeueCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        let name = names[indexPath.row]
+        cell.textLabel?.text = name
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavBar()
 
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension HabitsTableViewController{
+    func setupNavBar(){
+        title = "Habitual"
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(pressAddHabit(_:)))
+    }
+    
+    @objc func pressAddHabit(_ sender: UIBarButtonItem){
+        names.insert("Hello World!", at: 0)
+        let topIndexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [topIndexPath], with: .automatic)
+    }
 }
