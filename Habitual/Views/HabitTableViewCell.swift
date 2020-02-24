b18579e29051279785d05927f1565c9e45dab373
Henry Calderon <theceintiant@gmail.com>
2020-02-23 18:12:10 -0800

    Ch6 step - Creating Custom Cell |

---
+++ Habitual/Views/HabitTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HabitTableViewCell.swift
+//  Habitual
+//
+//  Created by Henry Calderon on 2/23/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class HabitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
